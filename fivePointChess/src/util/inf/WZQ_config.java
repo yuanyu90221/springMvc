59d6c517b1420d4f86e8d16717c28451c332b909
yuanyu90221 <yuanyu90221@gmail.com>
2015-11-25 02:00:58 +0800

    fivePointChess

---
+++ fivePointChess/src/util/inf/WZQ_config.java
@@ -0,0 +1,30 @@
+package util.inf;
+
+public interface WZQ_config {
+	/**
+	 * 棋盤配置格數
+	 */
+	public static final int Row = 15;
+	public static final int Column = 15;
+	
+	/**
+	 * 設置棋盤初始位置座標
+	 */
+	public static final int X = 30;
+	public static final int Y = 60;
+	
+	/**
+	 * 設置棋子大小
+	 */
+	public static final int ChessSize = 40;
+	
+	/**
+	 * 設置棋盤格子大小
+	 */
+	public static final int WZQ_size = 40;
+	
+	/**
+	 * Dimension size
+	 */
+	public static final int dimSize = 650;
+}
